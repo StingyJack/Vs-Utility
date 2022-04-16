8e05e33e658676c3a8e7c32087a086bf95ca1e08
Andrew Stanton <Andrew.Stanton@gep.com>
2022-04-16 16:23:54 -0400

    Adding revert instruction

--- Add-ResharperToggleButton.ps1
+++ Add-ResharperToggleButton.ps1
@@ -13,33 +13,36 @@
 
 #>
 $ErrorActionPreference = 'Stop'
-function Add-ResharperToggleButton
-{
+function Add-ResharperToggleButton {
     [CmdletBinding()]
     Param(
         [object] $VisualStudioDTE
     )
 
-    if ($Host.Name -ine "Package Manager Host"){
+    if ($Host.Name -ine "Package Manager Host") {
         throw "This must be run from the Nuget Package Manager Console in order to access the necessary types"
     }
 
-	$typePath = $VisualStudioDTE.GetType().Assembly.Location
-	Write-Host "Adding dte types if not already present from `n`t '$typePath'"
+    Write-Host "If this fails for any reason, delete the toolbar button by right clicking the toolbar, choosing Customize, selecting it and clickcing delete."
+    $typePath = $VisualStudioDTE.GetType().Assembly.Location
+    Write-Host "Adding dte types if not already present from `n`t '$typePath'"
     Add-Type -Path "$typePath"
     $cmdBarName = "R#"
     $cmdName = "ReSharper_ToggleSuspended"
     $cmdText = "R# Active"
     $toolbarType = [EnvDTE.vsCommandBarType]::vsCommandBarTypeToolbar
     
-	Write-Host "Creating command bar"
-    $cmdBar = $VisualStudioDTE.Commands.AddCommandBar($cmdBarName, $toolbarType)
-
-	Write-Host "Creating Command Item"
+    $cmdBar = $VisualStudioDTE.Commands | Where-Object { $_.Name -ieq $cmdBarName } | Select-Object -First 1
+    if ($null -eq $cmdBar) {
+        Write-Host "Creating command bar"
+        $cmdBar = $VisualStudioDTE.Commands.AddCommandBar($cmdBarName, $toolbarType)
+    }
+    
+    Write-Host "Creating Command Item"
     $cmdItem = $VisualStudioDTE.Commands.Item($cmdName).AddControl($cmdBar, 1)
 	
-	Write-Host "Setting item caption"
+    Write-Host "Setting item caption"
     $cmdItem.Caption = $cmdText
 
-	Write-Host "Done"
+    Write-Host "Done"
 }
