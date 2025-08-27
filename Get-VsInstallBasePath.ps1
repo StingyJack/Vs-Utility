548a29cb3aacfa431b33afefdf2b0b551b5cc4d7
Andrew Stanton <astanton1978@hotmail.com>
2025-08-27 09:47:15 -0400

    adding path checks to vswhere usage

--- Get-VsInstallBasePath.ps1
+++ Get-VsInstallBasePath.ps1
@@ -9,9 +9,21 @@ function Get-VsInstallBasePath
 
     )
     $vsWherePath = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
-
+    if (-not (Test-Path $vsWherePath))
+    {
+        throw "Could not find vswhere.exe at $vsWherePath"
+    }
     $installBasePath = (& $vsWherePath -latest -format json -utf8 | ConvertFrom-Json).installationPath
 
-    return $installBasePath
+    if ($null -eq $installBasePath)
+    {
+        throw "Could not get Visual Studio installation path from vswhere."
+    }
 
+    if (-not (Test-Path $installBasePath))
+    {
+        throw "Visual Studio installation path does not exist."
+    }
+
+    return $installBasePath
 }
